43fb5f0ad10ef39332f8f9a7448331bd168f8aef
Kamrul Hasan <khrahat92@gmail.com>
2022-08-12 01:44:12 +0600

    apply a error handler globally

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,22 @@ const userRoutes = require('./routes/userRoutes')
 app.use(express.json());
 app.use('/api/v1/admin',userRoutes )
 app.all('*',(req,res,next) => {
-    res.status(404).json({
-        status:'fail',
-        message: `not found ${req.originalUrl} on this server`
+    // res.status(404).json({
+    //     status:'fail',
+    //     message: `not found ${req.originalUrl} on this server`
+    // })
+    const err = new Error(`not found : ${req.originalUrl} on this server`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err)
+})
+
+app.use((err,req,res,next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({
+        status: err.status,
+        message:err.message
     })
 })
 
