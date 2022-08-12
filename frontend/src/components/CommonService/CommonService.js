8c88a3d22c9abb20110cb6e8154caa836ebd33b5
Kamrul Hasan <khrahat92@gmail.com>
2022-08-13 00:40:44 +0600

    add dynamic list with src functionallity

--- frontend/src/components/CommonService/CommonService.js
+++ frontend/src/components/CommonService/CommonService.js
@@ -2,12 +2,38 @@ import React from 'react';
 import './CommonService.scss';
 import CommonServiceDiv from '../CommonServiceDiv/CommonServiceDiv';
 const CommonService = () => {
-    const services = ['ই সেবা','পিডি এস(সহকারি মাধ্যমিক)','ভর্তি পরিক্ষার আবেদন']
-    const links = ['শিক্ষা মন্ত্রনালয়','মাধ্যমিক ও উচ্চশিক্ষা অধিদপ্তর','ব্যানবেজ','নায়েম','এনসিটিবি']
+    const services = [
+        {
+            title:'ই সেবা',
+            src: '/eseba'
+        },
+        {
+            title:'পিডি এস(সহকারি মাধ্যমিক)',
+            src: '/pisi'
+        },
+        {
+            title:'ভর্তি পরিক্ষার আবেদন',
+            src: '/admission'
+        }
+    ]
+    const links = [
+        {
+            title : 'শিক্ষা মন্ত্রনালয়',
+            src :'/shikhkha'
+        },
+        {
+            title : 'মাধ্যমিক ও উচ্চশিক্ষা অধিদপ্তর',
+            src :'/madhdhomik'
+        },
+        {
+            title : 'ব্যানবেজ',
+            src :'/banbase'
+        }
+    ]
     return (
         <div>
             <CommonServiceDiv headLine= 'অভ্যন্তরিন সেবা' services={services}/>
-            <CommonServiceDiv headLine= 'অভ্যন্তরিন সেবা' services={links}/>
+            <CommonServiceDiv headLine= 'গুরুত্বপূর্ণ লিংক' services={links}/>
         </div>
     );
 };
